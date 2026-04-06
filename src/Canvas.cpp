33bf8cd08a9c83e6abb8cc89bffda75f4bef1058
Alessandro Perez <alessandrogperez@gmail.com>
2026-04-06 18:06:12 +0200

    refactor: substitute asserts for if statement (code runs even if out-of-bounds)

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -10,16 +10,17 @@
 void writePixel(canvas &c, int x, int y, color col)
 {
     // Basic canvas boundary check
-    assert(x >= 0 && x < c.width && y >= 0 && y < c.height);
+    if(x >= 0 && x < c.width && y >= 0 && y < c.height)
         c.pixels[(y * c.width) + x] = col;
 }
 
 color pixelAt(const canvas &c, int x, int y)
 {
     // Basic canvas boundary check
-    assert(x >= 0 && x < c.width && y >= 0 && y < c.height);
-    
-    return c.pixels[(y * c.width) + x];
+    if(x >= 0 && x < c.width && y >= 0 && y < c.height)
+        return c.pixels[(y * c.width) + x];
+
+    return color(1,1,1);
 }
 
 int scaleColor(float color_float) {
