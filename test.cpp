c10e51e51578f6ac3504b5a841b199aa2e0bcbb6
Alessandro Perez <alessandrogperez@gmail.com>
2026-04-14 21:59:57 +0200

    test: add test.cpp (main file) for manual testing

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include "includes/Canvas.hpp"
+#include "includes/Colors.hpp"
+#include "includes/Tuple.hpp"
+
+/**
+ * Manual compilation:
+ *  g++ -std=c++20 main.cpp src/Canvas.cpp src/Colors.cpp src/Tuple.cpp src/Utils.cpp  
+ */
+
+
+int main(int argc, char **argv)
+{
+    color red(1, 0, 0);
+    canvas c(100, 100, red);
+
+    std::string ppmContent = canvasToPPM(c);
+
+    savePPM("test_output", ppmContent);
+
+    return 0;
+}
